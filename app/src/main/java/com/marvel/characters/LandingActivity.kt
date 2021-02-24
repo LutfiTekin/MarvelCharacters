53d5b42783522a1c58bd409a8444fcf44d075c94
Lütfi Tekin <lutfi-tekin@outlook.com>
2021-02-24 23:17:46 +0300

    Landing Activity

---
+++ app/src/main/java/com/marvel/characters/LandingActivity.kt
@@ -0,0 +1,30 @@
+package com.marvel.characters
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+
+class LandingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        Firebase.remoteConfig.apply {
+            setConfigSettingsAsync(configSettings)
+            setDefaultsAsync(R.xml.remote_config)
+            fetchAndActivate().addOnCompleteListener {
+                redirect()
+            }
+        }
+    }
+
+
+    private fun redirect(){
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
